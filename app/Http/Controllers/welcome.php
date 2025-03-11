641da6491b197ad8d866ad0dc3fc5ff470b6070a
panashe78 <161289848+panashe78@users.noreply.github.com>
2025-03-11 16:40:17 +0200

    Update welcome.php

--- app/Http/Controllers/welcome.php
+++ app/Http/Controllers/welcome.php
@@ -9,12 +9,125 @@ use Illuminate\Http\Request;
 
 class welcome extends Controller
 {
-
+    //
     public function index()
-{
-    // Redirect to the welcome page
-    return redirect()->route('welcome'); // Assuming 'welcome' is the name of your welcome route
-}
+    {
+        $restaurants = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','business')
+                       ->paginate(8);
+
+        $hrestaurants = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','business')
+                       ->where('city','Harare')
+                       ->paginate(8);
+
+        $brestaurants = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','business')
+                       ->where('city','Bulawayo')
+                       ->paginate(8);
+
+        $mrestaurants = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','business')
+                       ->where('city','Mutare')
+                       ->paginate(8);
+
+        $grestaurants = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','business')
+                       ->where('city','Gweru')
+                       ->paginate(8);
+
+        $hbars = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','restaurant')
+                       ->where('city','Harare')
+                       ->paginate(8);
+
+        $mbars = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','restaurant')
+                       ->where('city','Mutare')
+                       ->paginate(8);
+
+        $bbars = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','restaurant')
+                       ->where('city','Bulawayo')
+                       ->paginate(8);
+
+        $gbars = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','restaurant')
+                       ->where('city','Gweru')
+                       ->paginate(8);
+
+        $bars = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','restaurant')
+                       ->paginate(8);
+
+        $hbars = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','restaurant')
+                       ->where('city','Harare')
+                       ->paginate(12);
+
+        $hacco = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','accommodation')
+                       ->where('city','Harare')
+                       ->paginate(12);
+
+        $macco = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','accommodation')
+                       ->where('city','Mutare')
+                       ->paginate(8);
+
+        $bacco = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','accommodation')
+                       ->where('city','Bulawayo')
+                       ->paginate(8);
+
+        $gacco = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','accommodation')
+                       ->where('city','Gweru')
+                       ->paginate(8);
+
+        $bars = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','accommodation')
+                       ->paginate(8);
+
+                       
+       
+                    
+        return view('welcome', [
+            'restaurants' => $restaurants,
+            'bars' => $bars,
+            'mrestaurants' => $mrestaurants,
+            'hrestaurants' => $hrestaurants,
+            'brestaurants' => $brestaurants,
+            'grestaurants' => $grestaurants, 
+            'hbars' => $hbars,
+            'mbars' => $mbars, 
+            'bbars' => $bbars,
+            'gbars' => $gbars, 
+            'hacco' => $hacco,  
+            'macco' => $macco,
+            'bacco' => $bacco,
+            'gacco' => $gacco,
+
+
+        ]);
+    }
+
     public function show($id)
     {
         $restaurant = DB::table('restaurants')->where('id', $id)->first();
