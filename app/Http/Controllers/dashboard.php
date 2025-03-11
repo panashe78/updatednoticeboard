947ab67385b6f2c99f541aac63082b2d08bc7c7e
panashe78 <161289848+panashe78@users.noreply.github.com>
2025-03-11 16:44:11 +0200

    Update dashboard.php

--- app/Http/Controllers/dashboard.php
+++ app/Http/Controllers/dashboard.php
@@ -1,45 +1,144 @@
-<?php
 
+<?php
 namespace App\Http\Controllers;
 
-use App\Models\booking;
-use App\Models\Event;
-use App\Models\FAQ;
-use App\Models\menu;
-use App\Models\Message;
-use App\Models\Reservations;
-use App\Models\Restaurant;
-use App\Models\testimonial;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB; 
 use Illuminate\Http\Request;
 
-class dashboard extends Controller
+class welcome extends Controller
 {
     //
-
     public function index()
     {
-        $resorts = Restaurant::all();
-        $booking = booking::count();
-        $events = Event::count();
-        $mesages = Message::count();
-        $menu = menu::count();
-        $reservations = Reservations::count();
-        $restaurants = Restaurant::count();
-        $testimonials = testimonial::count();
-        $questions = FAQ::count();
-        
+        $restaurants = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','business')
+                       ->paginate(8);
 
+        $hrestaurants = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','business')
+                       ->where('city','Harare')
+                       ->paginate(8);
 
-        return view('dashboard', [
-            'booking' => $booking,
-            'events' => $events,
-            'mesages' => $mesages,
-            'menu' => $menu,
-            'reservations' => $reservations,
+        $brestaurants = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','business')
+                       ->where('city','Bulawayo')
+                       ->paginate(8);
+
+        $mrestaurants = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','business')
+                       ->where('city','Mutare')
+                       ->paginate(8);
+
+        $grestaurants = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','business')
+                       ->where('city','Gweru')
+                       ->paginate(8);
+
+        $hbars = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','restaurant')
+                       ->where('city','Harare')
+                       ->paginate(8);
+
+        $mbars = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','restaurant')
+                       ->where('city','Mutare')
+                       ->paginate(8);
+
+        $bbars = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','restaurant')
+                       ->where('city','Bulawayo')
+                       ->paginate(8);
+
+        $gbars = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','restaurant')
+                       ->where('city','Gweru')
+                       ->paginate(8);
+
+        $bars = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','restaurant')
+                       ->paginate(8);
+
+        $hbars = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','restaurant')
+                       ->where('city','Harare')
+                       ->paginate(12);
+
+        $hacco = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','accommodation')
+                       ->where('city','Harare')
+                       ->paginate(12);
+
+        $macco = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','accommodation')
+                       ->where('city','Mutare')
+                       ->paginate(8);
+
+        $bacco = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','accommodation')
+                       ->where('city','Bulawayo')
+                       ->paginate(8);
+
+        $gacco = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','accommodation')
+                       ->where('city','Gweru')
+                       ->paginate(8);
+
+        $bars = DB::table('restaurants')
+                       ->where('approval', true)
+                       ->where('description','accommodation')
+                       ->paginate(8);
+
+                       
+       
+                    
+        return view('welcome', [
             'restaurants' => $restaurants,
-            'testimonials' => $testimonials,
-            'questions' => $questions,
-            'resorts' => $resorts,
+            'bars' => $bars,
+            'mrestaurants' => $mrestaurants,
+            'hrestaurants' => $hrestaurants,
+            'brestaurants' => $brestaurants,
+            'grestaurants' => $grestaurants, 
+            'hbars' => $hbars,
+            'mbars' => $mbars, 
+            'bbars' => $bbars,
+            'gbars' => $gbars, 
+            'hacco' => $hacco,  
+            'macco' => $macco,
+            'bacco' => $bacco,
+            'gacco' => $gacco,
+
+
+        ]);
+    }
+
+    public function show($id)
+    {
+        $restaurant = DB::table('restaurants')->where('id', $id)->first();
+
+        if (!$restaurant) {
+            abort(404); // User not found
+        }
+
+        return view('restaurants.show', [
+            'restaurant' => $restaurant,
            
         ]);
     }
