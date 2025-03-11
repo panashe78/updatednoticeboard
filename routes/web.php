b948942943285305dc026ef74ba7923913182d66
panashe78 <161289848+panashe78@users.noreply.github.com>
2025-03-11 15:49:24 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,8 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/', function () {
-        return view('welcome');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
     })->name('dashboard');
 });
 
